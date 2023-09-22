ef8fed2152ca88a872d95c241014422108c6683e
robbertmijn <robbertmijn@gmail.com>
2023-09-22 15:56:43 +0200

    pps

--- batch_process.py
+++ batch_process.py
@@ -8,7 +8,7 @@ import multiprocessing as mp
 from itertools import product
 
 import analysis_utils as au
-SUBJECTS = [1, 2, 3, 4, 5, 6, 7]
+SUBJECTS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 # au.eet.read_subject.clear()
 # au.eet.autoreject_epochs.clear()
 # au.proc_subject.clear()
